4efb2dd010ee0a4c22f2a2c9eb358d4d35235e23
Fábio simones <simones.fabio1994@gmail.com>
2024-07-30 18:32:41 -0300

    Implementação de repositórios.

---
+++ CleanArcMvc.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,60 @@
+﻿using CleanArcMvc.Domain.Entities;
+using CleanArcMvc.Domain.Interfaces;
+using CleanArcMvc.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArcMvc.Infra.Data.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _productContext;
+
+        public ProductRepository(ApplicationDbContext productContext)
+        {
+            _productContext = productContext;
+        }
+
+        public async Task<Product> Create(Product product)
+        {
+            _productContext.Add(product);
+            await _productContext.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> GetProductById(int? id)
+        {
+            return await _productContext.Products.FindAsync(id);
+        }
+
+        public async Task<Product> GetProductCategoryAsync(int? id)
+        {
+            //eager loading
+            return await _productContext.Products.Include(c => c.Category)
+                .SingleOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _productContext.Products.ToListAsync();
+        }
+
+        public async Task<Product> Remove(Product product)
+        {
+            _productContext.Remove(product);
+            await _productContext.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> Update(Product product)
+        {
+            _productContext.Update(product);
+            await _productContext.SaveChangesAsync();
+            return product;
+        }
+    }
+}
