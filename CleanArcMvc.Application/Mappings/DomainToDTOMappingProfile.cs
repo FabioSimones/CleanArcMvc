933ad4c12db321e2bbe5ee24c1aab3aec826bd5c
Fábio simones <simones.fabio1994@gmail.com>
2024-08-01 17:40:51 -0300

    Criando os DTOs e os mapeamentos com o AutoMapper.

---
+++ CleanArcMvc.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CleanArcMvc.Application.DTOs;
+using CleanArcMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArcMvc.Application.Mappings
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<Product, ProductDTO>().ReverseMap();
+        }
+    }
+}
