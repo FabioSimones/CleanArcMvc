e45e76cb87bded4da0706ee52265d4a5aafd6854
Fábio simones <simones.fabio1994@gmail.com>
2024-08-01 18:17:28 -0300

    Implementando services.

---
+++ CleanArcMvc.Application/Services/CategoryService.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using CleanArcMvc.Application.DTOs;
+using CleanArcMvc.Application.Interfaces;
+using CleanArcMvc.Domain.Entities;
+using CleanArcMvc.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArcMvc.Application.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<CategoryDTO>> GetCategories()
+        {
+            var categoriesEntity = await _categoryRepository.GetCategories();
+            return _mapper.Map<IEnumerable<CategoryDTO>>(categoriesEntity);
+
+        }
+
+        public async Task<CategoryDTO> GetById(int? id)
+        {
+            var categoryEntity = await _categoryRepository.GetCategoryById(id);
+            return _mapper.Map<CategoryDTO>(categoryEntity);
+        }
+
+        public async Task Add(CategoryDTO categoryDTO)
+        {
+            var categoryEntity =  _mapper.Map<Category>(categoryDTO);
+            await _categoryRepository.Create(categoryEntity);
+        }
+        
+        public async Task Remove(int? id)
+        {
+            var categoryEntity = _categoryRepository.GetCategoryById(id).Result;
+            await _categoryRepository.Remove(categoryEntity);
+        }
+
+        public async Task Update(CategoryDTO categoryDTO)
+        {
+            var categoryEntity = _mapper.Map<Category>(categoryDTO);
+            await _categoryRepository.Update(categoryEntity);
+        }
+    }
+}
---
+++ CleanArcMvc.Application/Services/ProductService.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using CleanArcMvc.Application.DTOs;
+using CleanArcMvc.Application.Interfaces;
+using CleanArcMvc.Domain.Entities;
+using CleanArcMvc.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArcMvc.Application.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public ProductService(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository ??
+                throw new ArgumentNullException(nameof(productRepository));
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ProductDTO>> GetProducts()
+        {
+            var productsEntity = await _productRepository.GetProducts();
+            return _mapper.Map<IEnumerable<ProductDTO>>(productsEntity);
+        }
+
+        public async Task<ProductDTO> GetById(int? id)
+        {
+            var productEntity = await _productRepository.GetProductById(id);
+            return _mapper.Map<ProductDTO>(productEntity);
+        }
+
+        public async Task<ProductDTO> GetProductCategory(int? id)
+        {
+            var productEntity = await _productRepository.GetProductCategoryAsync(id);
+            return _mapper.Map<ProductDTO>(productEntity);
+        }
+
+        public async Task Add(ProductDTO productDTO)
+        {
+            var productEntity = _mapper.Map<Product>(productDTO);
+            await _productRepository.Create(productEntity);
+        }
+
+        public async Task Remove(int? id)
+        {
+            var productEntity = _productRepository.GetProductById(id).Result;
+            await _productRepository.Remove(productEntity);
+        }
+
+        public async Task Update(ProductDTO productDTO)
+        {
+            var productEntity = _mapper.Map<Product>(productDTO);
+            await _productRepository.Update(productEntity);
+        }
+    }
+}
