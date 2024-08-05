98e3fb88481037f23f401efa74b96f16f7c9bc5c
Fábio simones <simones.fabio1994@gmail.com>
2024-08-05 16:05:28 -0300

    CQRS - Implementando os Commands e as Queries para Products

---
+++ CleanArcMvc.Application/Products/Commands/ProductCommand.cs
@@ -0,0 +1,21 @@
+﻿using CleanArcMvc.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArcMvc.Application.Products.Commands
+{
+    public abstract class ProductCommand : IRequest<Product>
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+        public string Image { get; private set; }
+        public int CategoryId { get; set; }
+    }
+}
