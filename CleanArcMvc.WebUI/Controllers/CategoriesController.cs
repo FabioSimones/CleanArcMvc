4e9c852d5ba4e3cb6e25131b8d5b1e2d4b5ed4fa
Fábio simones <simones.fabio1994@gmail.com>
2024-08-06 17:55:00 -0300

    Alteração de barra de navegação. Adicionado CRUD para os itens de Categorias.

--- CleanArcMvc.WebUI/Controllers/CategoriesController.cs
+++ CleanArcMvc.WebUI/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
-﻿using CleanArcMvc.Application.Interfaces;
+﻿using CleanArcMvc.Application.DTOs;
+using CleanArcMvc.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -19,5 +20,81 @@ namespace CleanArcMvc.WebUI.Controllers
             var categories = await _categoryService.GetCategories();
             return View(categories);
         }
+
+        [HttpGet()]
+        public  ActionResult Create()
+        {            
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryDTO category)
+        {
+            if (ModelState.IsValid)
+            {
+                await _categoryService.Add(category);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null) return NotFound();
+
+            var categoryDto = await _categoryService.GetById(id);
+
+            if(categoryDto == null) return NotFound();
+
+            return View(categoryDto);
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> Edit(CategoryDTO categoryDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _categoryService.Update(categoryDTO);
+                }
+                catch (System.Exception)
+                {
+
+                    throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoryDTO);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var categoryDto = await _categoryService.GetById(id);
+
+            if (categoryDto == null) return NotFound();
+
+            return View(categoryDto);
+        }
+
+        [HttpPost(), ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _categoryService.Remove(id);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+            var categoryDto = await _categoryService.GetById(id);
+            if (categoryDto == null) return NotFound();
+            return View(categoryDto);
+        }
+
     }
 }
