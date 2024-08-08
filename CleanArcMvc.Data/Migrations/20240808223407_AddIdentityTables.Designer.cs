f99cc2a5a3f9954e135382a33dea61ccc93b92b4
Fábio simones <simones.fabio1994@gmail.com>
2024-08-08 19:35:47 -0300

    Criando tabelas do Identity.

--- CleanArcMvc.Data/Migrations/20240808223407_AddIdentityTables.Designer.cs
+++ CleanArcMvc.Data/Migrations/20240808223407_AddIdentityTables.Designer.cs
@@ -4,14 +4,16 @@ using CleanArcMvc.Infra.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CleanArcMvc.Infra.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240808223407_AddIdentityTables")]
+    partial class AddIdentityTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
