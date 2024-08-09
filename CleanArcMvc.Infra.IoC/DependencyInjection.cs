bb0f744780bfc0fb0c512db39ec0157c7786d4fc
Fábio simones <simones.fabio1994@gmail.com>
2024-08-09 17:56:31 -0300

    Incluindo os serviços do Identity e registrando os serviços no projeto Infra.IoC.

--- CleanArcMvc.Infra.IoC/DependencyInjection.cs
+++ CleanArcMvc.Infra.IoC/DependencyInjection.cs
@@ -1,10 +1,13 @@
 ﻿using CleanArcMvc.Application.Interfaces;
 using CleanArcMvc.Application.Mappings;
 using CleanArcMvc.Application.Services;
+using CleanArcMvc.Domain.Account;
 using CleanArcMvc.Domain.Interfaces;
 using CleanArcMvc.Infra.Data.Context;
+using CleanArcMvc.Infra.Data.Identity;
 using CleanArcMvc.Infra.Data.Repositories;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -21,11 +24,21 @@ namespace CleanArcMvc.Infra.IoC
                 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
                 b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)
             ));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(options => options.AccessDeniedPath = "/Account/Login");
+
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped<IProductService, ProductService>();
             services.AddScoped<ICategoryService, CategoryService>();
             services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            services.AddScoped<IAuthenticate, AuthenticateService>();
+            services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
 
             var myHandlers = AppDomain.CurrentDomain.Load("CleanArcMvc.Application");
             services.AddMediatR(myHandlers);
