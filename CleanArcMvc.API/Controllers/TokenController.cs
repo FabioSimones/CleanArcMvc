4434181291e817a17e41fbb5015e34dcbfa28311
Fábio simones <simones.fabio1994@gmail.com>
2024-08-12 17:42:02 -0300

    Implementando Models e iniciando Controller para o TokenJWT.

---
+++ CleanArcMvc.API/Controllers/TokenController.cs
@@ -0,0 +1,38 @@
+﻿using CleanArcMvc.API.Models;
+using CleanArcMvc.Domain.Account;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace CleanArcMvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly IAuthenticate _authenticate;
+
+        public TokenController(IAuthenticate authenticate)
+        {
+            _authenticate = authenticate ??
+                throw new ArgumentNullException(nameof(authenticate));
+        }
+
+        [HttpPost("LoginUser")]
+        public async Task<ActionResult<UserToken>> Login([FromBody] LoginModel userInfo)
+        {
+            var result = await _authenticate.Authenticate(userInfo.Email, userInfo.Password);
+            if (result)
+            {
+                //return GenerateToken(userInfo);
+                return Ok($"User {userInfo.Email} login successfully.");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attemp");
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
