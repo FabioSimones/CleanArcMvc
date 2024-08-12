1b284d7f807bd6533fa749d8f8e31ea5f4eb9e9d
Fábio simones <simones.fabio1994@gmail.com>
2024-08-12 17:57:40 -0300

    Implementando método para geração de token.

--- CleanArcMvc.API/Controllers/TokenController.cs
+++ CleanArcMvc.API/Controllers/TokenController.cs
@@ -2,7 +2,12 @@
 using CleanArcMvc.Domain.Account;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace CleanArcMvc.API.Controllers
@@ -12,9 +17,11 @@ namespace CleanArcMvc.API.Controllers
     public class TokenController : ControllerBase
     {
         private readonly IAuthenticate _authenticate;
+        private readonly IConfiguration _configuration;
 
-        public TokenController(IAuthenticate authenticate)
+        public TokenController(IAuthenticate authenticate, IConfiguration configuration)
         {
+            _configuration = configuration;
             _authenticate = authenticate ??
                 throw new ArgumentNullException(nameof(authenticate));
         }
@@ -25,14 +32,51 @@ namespace CleanArcMvc.API.Controllers
             var result = await _authenticate.Authenticate(userInfo.Email, userInfo.Password);
             if (result)
             {
-                //return GenerateToken(userInfo);
-                return Ok($"User {userInfo.Email} login successfully.");
+                return GenerateToken(userInfo);
+                //return Ok($"User {userInfo.Email} login successfully.");
             }
             else
             {
                 ModelState.AddModelError(string.Empty, "Invalid login attemp");
                 return BadRequest(ModelState);
             }
+        }
+
+        private UserToken GenerateToken(LoginModel userInfo)
+        {
+            //Declaração do usuário
+            var claims = new[]
+            {
+                new Claim("email", userInfo.Email),
+                new Claim("meuvalor", "o que voce quiser"),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            //Gerar a chave privada para assinar o token
+            var privateKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration["Jwt:SecretKey"]));
+
+            //Gerar a assinatura digital
+            var credentials = new SigningCredentials(privateKey, SecurityAlgorithms.HmacSha256);
+
+            //definir tempo de expiração
+            var expiration = DateTime.UtcNow.AddMinutes(10);
+
+            //Gerar o token
+            JwtSecurityToken token = new JwtSecurityToken(
+                    issuer: _configuration["Jwt:Issuer"],
+                    audience: _configuration["Jwt:Audience"],
+                    claims: claims,
+                    expires: expiration,
+                    signingCredentials: credentials
+
+            );
+
+            return new UserToken()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = expiration
+            };
         }
     }
 }
